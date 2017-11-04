8cc9987ce12f01b2599b8fd24766a4d446dba5e1
Timofei Korostelev <lincosmail@gmail.com>
2017-11-04 11:42:17 -0700

    Added homework proofs

--- submit_homework.sh
+++ submit_homework.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-hdfs dfs -copyFromLocal /spark-core /spark-core
+# hdfs dfs -copyFromLocal /spark-core /spark-core
 TEST_RESOURCES='/spark-core/dataset/local/'
 hdfs dfs -rm -r /results
 spark-submit --conf spark.ui.port=10500 --class com.epam.hubd.spark.scala.core.homework.MotelsHomeRecommendation target/spark-sql-hw.jar $TEST_RESOURCES/bids.txt $TEST_RESOURCES/motels.txt $TEST_RESOURCES/exchange_rate.txt /results
-hdfs dfs -cat /results/aggregated/* | head -1000
+hdfs dfs -cat /results/aggregated/* | head -100
